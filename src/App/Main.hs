0f7d45261b844880dc4edf8e21748b49ef5e1fbc
pf03email <pf03@mail.ru>
2021-05-18 15:47:50 +0300

    threads delete

--- src/App/Main.hs
+++ src/App/Main.hs
@@ -1,7 +1,5 @@
 module App.Main where
 
-import Control.Concurrent (MVar, ThreadId, forkIO, newEmptyMVar, putMVar, takeMVar, threadDelay, throwTo)
-import Control.Exception.Base (AsyncException (ThreadKilled, UserInterrupt), finally)
 import qualified Logic.Bot as Bot (application)
 import Logic.Config (App (Telegram, VK))
 import T.State (T)
@@ -12,18 +10,7 @@ import qualified VK.Bot as VK
 
 main_ :: IO ()
 main_ = do
-  appExitMVar <- newEmptyMVar
-  appIdMVar <- newEmptyMVar
-  exitId <- forkIO $ exit appIdMVar
-
-  threadDelay 2000000
-  appId <- forkIO $ do
-    finally (runT switchApplication) $ do
-      putStrLn "Application closed"
-      throwTo exitId ThreadKilled
-      putMVar appExitMVar ()
-  putMVar appIdMVar appId
-  takeMVar appExitMVar
+  runT switchApplication
   putStrLn "Press enter for exit..."
   _ <- getLine
   return ()
@@ -34,16 +21,3 @@ switchApplication = do
   case app of
     VK -> Bot.application VK.Pointer
     Telegram -> Bot.application Telegram.Pointer
-
-exit :: MVar ThreadId -> IO ()
-exit appIdMVar = do
-  putStrLn "Press q to exit..."
-  line <- getLine
-  if line == "q"
-    then do
-      putStrLn "Exit from application when long polling request ends ..."
-      appId <- takeMVar appIdMVar
-      throwTo appId UserInterrupt
-    else do
-      putStrLn "Wrong command"
-      exit appIdMVar
