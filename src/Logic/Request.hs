25da14c4b12c26b2cfd5029d6118b5103a5b8f56
pf03email <pf03@mail.ru>
2021-07-19 10:16:15 +0300

    add parseRequest function

--- src/Logic/Request.hs
+++ src/Logic/Request.hs
@@ -3,10 +3,10 @@
 
 module Logic.Request where
 
-import Class (IAPI, MCache, MIOError, MLog)
+import Class --(IAPI, MCache, MIOError, MLog)
 import Common.Types (Host (..), Path (..))
 import Control.Concurrent (threadDelay)
-import Control.Monad.State.Lazy (when)
+import Control.Monad.State.Lazy (when)                                      
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
@@ -15,6 +15,7 @@ import qualified Interface.Error.Exports as Error
 import qualified Interface.Log.Exports as Log
 import qualified Messenger.API.Class as API
 import qualified Network.HTTP.Simple as HTTP
+import Control.Monad.Catch ( SomeException )
 
 -- | Low level wrapper for request
 sendRequest :: (MLog m, MIOError m) => HTTP.Request -> Bool -> m LC.ByteString
@@ -31,7 +32,7 @@ sendRequest request save = do
       return jsonBody
     else do
       Log.writeErrorM "Request failed with error"
-      Log.writeErrorM $ show response
+      Log.writeErrorM $ show response                                                                                              
       Error.throw $ Error.QueryError "Request failed with error"
   where
     getResponse :: (MLog m, MIOError m) => m (HTTP.Response LC.ByteString)
@@ -57,6 +58,15 @@ sendApiRequest api query save = do
   let request = buildRequest host path query
   sendRequest request save
 
+buildRequestWithInit :: HTTP.Request -> HTTP.Query -> HTTP.Request
+buildRequestWithInit initRequest query =
+  HTTP.setRequestSecure True $
+    HTTP.setRequestMethod "POST" $
+      HTTP.setRequestPort 443 $
+            HTTP.setRequestBodyURLEncoded
+              (map (\(a, Just b) -> (a, b)) query)
+              initRequest
+
 buildRequest :: Host -> Path -> HTTP.Query -> HTTP.Request
 buildRequest (Host host) (Path path) query =
   HTTP.setRequestSecure True $
@@ -67,3 +77,10 @@ buildRequest (Host host) (Path path) query =
             HTTP.setRequestBodyURLEncoded
               (map (\(a, Just b) -> (a, b)) query)
               HTTP.defaultRequest
+
+parseRequest  :: MError m => String -> m HTTP.Request
+parseRequest str = do
+  let eRequest = HTTP.parseRequest str :: Either SomeException HTTP.Request
+  case eRequest of 
+    Left err -> Error.throw $ Error.QueryError (show err)
+    Right request -> return request
--- src/Messenger/Bot/VK/Instances.hs
+++ src/Messenger/Bot/VK/Instances.hs
@@ -1,12 +1,9 @@
 module Messenger.Bot.VK.Instances where
 
-import Class (MError, MTrans)
-import Common.Functions (template)
-import Common.Types (Host (..), Label, Path (..))
-import Data.List.Split (splitOn)
+import Class (MTrans)
+import Common.Types (Label)
 import Data.Maybe (fromMaybe)
 import qualified Interface.Cache.Exports as Cache
-import qualified Interface.Error.Exports as Error
 import qualified Interface.Log.Exports as Log
 import qualified Logic.Request as Request
 import qualified Logic.VK.Query.Functions as Query
@@ -39,8 +36,8 @@ getUpdates :: MTrans m => Update.Init -> m ([Update.Update], Update.Init)
 getUpdates init@(Update.Init server _ ts) = do
   Log.setSettings Color.Cyan True "getUpdates"
   let query = Query.longPollQuery init 25
-  (host, path) <- parseServer server
-  let request = Request.buildRequest host path query
+  initRequest <- Request.parseRequest server
+  let request = Request.buildRequestWithInit initRequest query
   Log.writeSending
   json <- Request.sendRequest request True -- long polling
   Log.writeReceiving
@@ -65,21 +62,3 @@ sendMessage update btns = do
   Log.writeReceiving
   object <- Parse.getObject json
   Log.writeReceivingData "object" object
-
--- "https://lp.vk.com/wh777777777" -> "lp.vk.com" "/wh777777777"
-parseServer :: MError m => String -> m (Host, Path)
-parseServer url = do
-  let err =
-        Error.QueryError $
-          template "Could not match pattern \"https://{host}/{path}\" in long polling server \"{0}\"" [url]
-  let strs = splitOn "//" url
-  case strs of
-    [https, str] -> do
-      if https /= "https:"
-        then Error.throw err
-        else do
-          let strs1 = splitOn "/" str
-          case strs1 of
-            [host, path] -> return (Host host, Path $ '/' : path)
-            _ -> Error.throw err
-    _ -> Error.throw err
