0a0d133e5af83e07dcf6e42b7602e03db6e07d51
pf03email <pf03@mail.ru>
2021-08-13 10:34:37 +0300

    deleted unused modules

--- src/Interface/MTrans.hs
+++
@@ -1,8 +0,0 @@
-module Interface.MTrans where
-
--- import Interface.Cache.Class as Cache (MIOCache)
--- import Interface.Error.Class as Error (MError)
--- import Interface.Log.Class as Log (MLog)
--- import Control.Monad.IO.Class (MonadIO)
-
--- class (MIOCache m, MonadIO m) => MTrans m
